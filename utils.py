ce29011da9f0001b6bce481d4b0f93820dc31c70
aleatorio-conta <contaaleatorio208@gmail.com>
2026-06-05 13:38:06 -0300

    Estilo para o terminal

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from UI import console, criar_painel
+
 
 def formatar_valor(campo, valor):
     if campo == "data_hora":
@@ -8,25 +10,32 @@ def formatar_valor(campo, valor):
             return str(valor)
     return str(valor)
 
+
 def exibir_tabela(lista):
     if not lista:
-        print("Nenhum item encontrado.")
+        criar_painel("Nenhum item encontrado.", "Aviso", estilo="yellow")
         return
+
     campos = list(lista[0].keys())
     larguras = {}
+
     for c in campos:
         larguras[c] = max(len(c), max(len(formatar_valor(c, i[c])) for i in lista))
 
     separador = "+-" + "-+-".join("-" * larguras[c] for c in campos) + "-+"
-    cabecalho  = "| " + " | ".join(c.upper().ljust(larguras[c]) for c in campos) + " |"
+    cabecalho = "| " + " | ".join(c.upper().ljust(larguras[c]) for c in campos) + " |"
 
-    print(separador)
-    print(cabecalho)
-    print(separador)
+    console.print(f"[cyan]{separador}[/cyan]")
+    console.print(f"[bold white]{cabecalho}[/bold white]")
+    console.print(f"[cyan]{separador}[/cyan]")
+
     for item in lista:
         linha = "| " + " | ".join(formatar_valor(c, item[c]).ljust(larguras[c]) for c in campos) + " |"
-        print(linha)
-    print(separador)
+        linha_estilizada = linha.replace("|", "[cyan]|[/cyan]")
+        console.print(linha_estilizada)
+
+    console.print(f"[cyan]{separador}[/cyan]")
+
 
 def converter(valor, tipo):
     try:
